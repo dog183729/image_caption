033956743c456a30a7557e9b232ce92523c7d5b8
Shuang Liu <shuangliu@Shuangs-Air.attlocal.net>
2024-08-17 10:59:56 -0700

    feat: add tag decoder

--- image_caption/model/blocks/caption_decoder.py
+++ image_caption/model/blocks/caption_decoder.py
@@ -2,38 +2,47 @@ import torch
 from torch import nn
 from transformers import GPT2LMHeadModel, GPT2Tokenizer
 
+from image_caption.data.dataloader import get_tokenizer
+
 
 class CaptionDecoder(nn.Module):
     def __init__(self):
         super(CaptionDecoder, self).__init__()
+        self.text_tokenizer = get_tokenizer()
         self.text_decoder = GPT2LMHeadModel.from_pretrained("distilgpt2")
 
-    def forward(self, vision_embedding, input_ids, attention_mask, labels):
+    def forward(self, vision_embedding, tag_names, input_ids, attention_mask, labels):
         """
         :param vision_embedding: batch_size * video_seq_len * 768
+        :param tag_names: list of positive tag_names
         :param input_ids: batch_size * text_seq_len
         :param attention_mask: batch_size * text_seq_len
         :param labels: batch_size * text_seq_len
         :return:
         """
+        tag_names = [",".join(tag_list) for tag_list in tag_names]  # ["tag1,tag2,tag3", "tag4,tag5", ...]
+        tag_inputs = self.text_tokenizer(tag_names, padding="longest", return_tensors="pt")
+        tag_input_ids, tag_attention_mask = tag_inputs.input_ids.to(vision_embedding.device), tag_inputs.attention_mask.to(vision_embedding.device)
+        tag_input_embeddings = self.text_decoder.transformer.wte(tag_input_ids)  # batch_size * tag_seq_len * 768
+
         # Text decoder input embeddings
         decoder_input_embeddings = self.text_decoder.transformer.wte(input_ids)  # batch_size * text_seq_len * 768
 
         # Combine vision embeddings and text input embeddings
-        combined_embedding = torch.cat([vision_embedding, decoder_input_embeddings],
-                                       dim=1)  # batch_size * (196 + text_seq_len) * 768
+        combined_embedding = torch.cat([vision_embedding, tag_input_embeddings, decoder_input_embeddings],
+                                       dim=1)  # batch_size * (196 + tag_seq_len + text_seq_len) * 768
 
         # TODO: append -100 to `label` on the left to make it have the shape of (batch_size, 196 + text_seq_len)
         # Append -100 to labels on the left to match the combined embedding shape
         extended_labels = torch.cat(
-            [torch.full((labels.size(0), vision_embedding.size(1)), -100).to(labels.device), labels],
-            dim=1)  # batch_size * (196 + text_seq_len)
+            [torch.full((labels.size(0), vision_embedding.size(1) + tag_input_embeddings.size(1)), -100).to(labels.device), labels],
+            dim=1)  # batch_size * (196 + tag_seq_len + text_seq_len)
 
         # TODO: append 1 to `attention_mask` on the left to make it have the shape of (batch_size, 196 + text_seq_len)
         # Append 1 to attention mask on the left to match the combined embedding shape
         extended_attention_mask = torch.cat(
-            [torch.ones(attention_mask.size(0), vision_embedding.size(1)).to(attention_mask.device), attention_mask],
-            dim=1)  # batch_size * (196 + text_seq_len)
+            [torch.ones(attention_mask.size(0), vision_embedding.size(1)).to(attention_mask.device), tag_attention_mask, attention_mask],
+            dim=1)  # batch_size * (196 + tag_seq_len + text_seq_len)
 
         # Text decoder forward pass
         text_output = self.text_decoder(inputs_embeds=combined_embedding, attention_mask=extended_attention_mask,
@@ -54,5 +63,6 @@ if __name__ == "__main__":
     model = CaptionDecoder()
 
     dummy_vision_embedding = torch.rand(1, 196, 768)
-    loss = model(vision_embedding=dummy_vision_embedding, input_ids=input_ids, attention_mask=attention_mask, labels=labels)
+    dummy_tag_names = [["tag1", "tag2", "tag3"]]
+    loss = model(vision_embedding=dummy_vision_embedding, tag_names=dummy_tag_names, input_ids=input_ids, attention_mask=attention_mask, labels=labels)
     print("Loss:", loss.item())
--- image_caption/model/model.py
+++ image_caption/model/model.py
@@ -1,17 +1,32 @@
 import torch
 from torch import nn
-from transformers import SiglipVisionModel, GPT2LMHeadModel, GPT2Tokenizer
+from transformers import GPT2Tokenizer
 
 from image_caption.model.blocks.vision_encoder import VisionEncoder
+from image_caption.model.blocks.tag_decoder import TagDecoder
 from image_caption.model.blocks.caption_decoder import CaptionDecoder
 
 class VisionLanguageModel(nn.Module):
-    def __init__(self):
+    def __init__(self, tag_embedding, ids_to_tags, d_model, ffn_hidden, n_head, n_layers, drop_prob):
         super(VisionLanguageModel, self).__init__()
         self.vision_encoder = VisionEncoder(d_model=768)
+        self.ids_to_tags = ids_to_tags
+        self.tag_decoder = TagDecoder(tag_embedding, d_model, ffn_hidden, n_head, n_layers, drop_prob)
         self.caption_decoder = CaptionDecoder()
 
-    def forward(self, input_ids, attention_mask, pixel_values, labels):
+    def postprocess(self, tag_logits, threshs):
+        probs = torch.sigmoid(tag_logits)  # N * num_tags
+        batch_size = probs.shape[0]
+
+        tag_lists = []
+        for i in range(batch_size):
+            tag_ids = torch.where(tag_logits[i] >= threshs)[0]
+            tag_names = [self.ids_to_tags[idx.item()] for idx in tag_ids]
+            tag_lists.append(tag_names)
+
+        return tag_lists
+
+    def forward(self, input_ids, attention_mask, pixel_values, labels, threshs=0.6):
         """
         :param input_ids: batch_size * text_seq_len
         :param attention_mask: batch_size * text_seq_len
@@ -22,12 +37,26 @@ class VisionLanguageModel(nn.Module):
         # Vision encoder forward pass
         vision_embedding = self.vision_encoder(pixel_values)
 
+        # Tag decoder forward path
+        tag_logits = self.tag_decoder(vision_embedding)  # N * num_tags
+
+        # Tag postprocess (tag_logits, thresholds) -> positive tag names
+        tag_names = self.postprocess(tag_logits, threshs)
+
         # Caption decoder forward path
-        caption_loss = self.caption_decoder(vision_embedding, input_ids=input_ids, attention_mask=attention_mask, labels=labels)
+        caption_loss = self.caption_decoder(vision_embedding, tag_names, input_ids=input_ids, attention_mask=attention_mask, labels=labels)
         return caption_loss
 
 
 if __name__ == "__main__":
+    from image_caption.data.dataset import COCOCaptionDataset
+
+    # init dataset
+    ann_path = "captions_train2017_with_tags_updated.json"
+    images_dir = "/Users/shuangliu/Downloads/data/coco/images/train2017"
+    dataset = COCOCaptionDataset(ann_path=ann_path, images_dir=images_dir)
+
+    # init caption inputs
     tokenizer = GPT2Tokenizer.from_pretrained("distilgpt2")
     tokens = tokenizer("A caption for the image.", return_tensors="pt")
     input_ids = tokens.input_ids
@@ -36,7 +65,9 @@ if __name__ == "__main__":
     # TODO: use `input_ids` as labels since shifting is implicitly handled by Huggingface.
     labels = torch.cat([input_ids[:, 1:], torch.tensor([[tokenizer.eos_token_id]], dtype=torch.long).to(input_ids.device)], dim=1)
 
-    model = VisionLanguageModel()
+    model = VisionLanguageModel(
+        tag_embedding=dataset.all_tags_embedding, ids_to_tags=dataset.ids_to_tags, d_model=768, ffn_hidden=2048, n_head=8, n_layers=6, drop_prob=0.1
+    )
 
     dummy_pixel_values = torch.rand(1, 3, 224, 224)
     loss = model(input_ids=input_ids, attention_mask=attention_mask, pixel_values=dummy_pixel_values, labels=labels)
